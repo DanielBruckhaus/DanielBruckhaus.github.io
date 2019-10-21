065fb28a97b76bf27263f1f4aba9530039ff4645
Bruckhaus Daniel <Daniel.Bruckhaus@orbis.de>
2019-10-21 23:14:28 +0200

    v1.1.1

--- xrm/jstb/version.js
+++ xrm/jstb/version.js
@@ -1,11 +1,10 @@
-const version = "1.0.1";
-const solutionVersion = "8.2.20190328";
-const releasedOn = "2019-03-28T08:49:51.934Z";
+const version = "1.1.1";
+const releasedOn = "2019-10-21T20:46:14.703Z";
 
 export {
   version,
-  solutionVersion,
   releasedOn
 };
 
 export default version;
+
